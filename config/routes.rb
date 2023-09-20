9bfa6d7aee83e6d4d349fd87f6e05e0aa7a405ba
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-09-20 16:03:18 +0500

    Add view for shopping list

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'public_recipes/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   get 'shopping_list/recipe_id/:recipe_id/inventory_id/:inventory_id', to: 'shopping_lists#shopping_list', as: 'shopping_list'
-  root 'users#index'
+  root 'public_recipes#index'
 end
