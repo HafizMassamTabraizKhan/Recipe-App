8b3cd94238719b34d0b534fa17520bd8e418b2db
Peter Dumbari <dummytwiz@gmail.com>
2023-09-20 11:33:04 +0100

    implemented the inventory functions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,3 +1,11 @@
 class Ability
   include CanCan::Ability
+
+  def initialize(user)
+    return unless user.present?
+
+    can :manage, Inventory, user_id: user.id
+    can :manage, Food, user_id: user.id
+    can :manage, InventoryFood, inventory: { user_id: user.id }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root 'users#index'
+
+  resources :inventories, only: [:index, :new, :show, :create, :update, :destroy] do
+    resources :inventory_foods, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+
+  end
 end
+
