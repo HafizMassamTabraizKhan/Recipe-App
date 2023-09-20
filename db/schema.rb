333bd8037afe89e4c4844199d7ab6a5f36a0d384
ProgramKingAbel <abelmorara254@gmail.com>
2023-09-21 01:52:48 +0300

    Migrate to string for preparation & cooking time

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_20_162255) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_20_224852) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,8 +53,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_20_162255) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
-    t.decimal "preparation_time"
-    t.decimal "cooking_time"
+    t.string "preparation_time"
+    t.string "cooking_time"
     t.text "description"
     t.boolean "public"
     t.bigint "user_id", null: false
