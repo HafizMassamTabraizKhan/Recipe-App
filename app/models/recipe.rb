db4387d560f498b8a1f0ae9b45291d2da937d25e
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-09-21 10:37:03 +0500

    Add validations to recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,11 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
   has_many :foods, through: :recipe_foods
+
+  attribute :public, :boolean, default: false
+
+  validates :name, presence: true
+  validates :preparation_time, presence: true
+  validates :cooking_time, presence: true
+  validates :description, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,28 +2,28 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { User.create(name: 'Massam', email: 'm@example.com', password: 'password') }
-  it 'is valid with valid attributes' do
+  it 'should valid with valid attributes' do
     expect(user.valid?).to eq true
   end
-  #   it 'should not create a user without a name' do
-  #     user = User.create(email: 'm@example.com', password: 'password')
-  #     expect(user.valid?).to eq false
-  #   end
-  it 'is not valid without a password' do
+  it 'should not create a user without a name' do
+    user = User.create(email: 'm@example.com', password: 'password')
+    expect(user.valid?).to eq false
+  end
+  it 'should not valid without a password' do
     user = User.create(name: 'Massam', email: 'm@example.com')
     expect(user).not_to be_valid
   end
-  it 'is not valid with the duplicate email' do
+  it 'should not valid with the duplicate email' do
     User.create(name: 'Peter', email: 'peter@example.com', password: 'password')
     user = User.new(name: 'Abel', email: 'peter@example.com', password: 'password')
     expect(user).not_to be_valid
   end
-  it 'is valid with a unique email' do
+  it 'should valid with a unique email' do
     User.create(name: 'Peter', email: 'peter@example.com', password: 'password')
     user = User.new(name: 'Abel', email: 'abel@example.com', password: 'password')
     expect(user).to be_valid
   end
-  it 'has many inventories' do
+  it 'should have many inventories' do
     association = described_class.reflect_on_association(:inventories)
     expect(association.macro).to eq :has_many
   end
