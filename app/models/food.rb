1cea50904aa7aa5b8868e001b29e509dfd90dfc5
ProgramKingAbel <abelmorara254@gmail.com>
2023-09-21 12:08:30 +0300

    Add validations for food

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,4 +2,15 @@ class Food < ApplicationRecord
   has_many :inventory_foods
   has_many :recipe_foods
   has_many :recipes, through: :recipe_foods
+
+  validates :name, uniqueness: true 
+  validate :name_is_unique_case_insensitive, on: :create 
+
+  private
+
+  def name_is_unique_case_insensitive
+    if Food.where('LOWER(name) = ?', name.downcase).exists?
+      errors.add(:name, 'has already been taken (case-insensitive)')
+    end
+  end
 end
