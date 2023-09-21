04b186bff5f73535787604f135d623bb09fe85db
Peter Dumbari <dummytwiz@gmail.com>
2023-09-21 23:39:24 +0100

    add integration test for shopping list

---
+++ app/controllers/shopping_lists_controller.rb
@@ -0,0 +1,57 @@
+class ShoppingListsController < ApplicationController
+  def shopping_list
+    @inventory_obj = Inventory.find(params[:selected_inventory_id])
+    @recipe_obj = Recipe.find(params[:recipe_id])
+
+    food_names_from_inventory = @inventory_obj.inventory_foods.map do |inventory_food|
+      inventory_food.food.name.downcase
+    end
+
+    @missing_foods = find_missing_foods(food_names_from_inventory)
+
+    @total_price = calculate_total_price(@missing_foods)
+  end
+
+  private
+
+  def find_missing_foods(food_names_from_inventory)
+    missing_foods = []
+
+    @recipe_obj.recipe_foods.each do |recipe_food|
+      inventory_name = recipe_food.food.name.downcase
+
+      if food_names_from_inventory.include?(inventory_name)
+        inventory_food = find_inventory_food(inventory_name)
+
+        if inventory_food.quantity < recipe_food.quantity
+          missing_foods << {
+            food: inventory_food.food,
+            quantity_needed: recipe_food.quantity - inventory_food.quantity
+          }
+        end
+      else
+        missing_foods << {
+          food: recipe_food.food,
+          quantity_needed: recipe_food.quantity
+        }
+      end
+    end
+
+    missing_foods
+  end
+
+  def find_inventory_food(inventory_name)
+    @inventory_obj.inventory_foods.find { |ifood| ifood.food.name.downcase == inventory_name }
+  end
+
+  def calculate_total_price(missing_foods)
+    total_price = 0
+
+    missing_foods.each do |missing_food|
+      price_multiply_with_qty = missing_food[:food].price * missing_food[:quantity_needed]
+      total_price += price_multiply_with_qty
+    end
+
+    total_price
+  end
+end
--- spec/features/shopping_list_spec.rb
+++ spec/features/shopping_list_spec.rb
@@ -6,52 +6,45 @@ RSpec.feature 'Shopping List Page', type: :feature do
   let(:user) { FactoryBot.create(:user) }
   let(:inventory) { FactoryBot.create(:inventory, user: user, name: 'Sample Inventory') }
   let(:recipe) { FactoryBot.create(:recipe, user: user, name: 'Sample Recipe') }
-  let(:food1) { FactoryBot.create(:food, name: 'Food 1', price: 5.0) }
-  let(:food2) { FactoryBot.create(:food, name: 'Food 2', price: 10.0) }
-  let(:food3) { FactoryBot.create(:food, name: 'Food 3', price: 15.0) }
+  let(:missing_foods) { [] }
 
   before do
     login_as(user)
-    recipe.recipe_foods.create(food: food1, quantity: 2)
-    recipe.recipe_foods.create(food: food2, quantity: 3)
-    inventory.inventory_foods.create(food: food2, quantity: 1)
 
-    # Stub the selected_inventory_id in params
-    allow_any_instance_of(ActionController::Parameters).to receive(:[]).with(:selected_inventory_id).and_return(inventory.id)
+    # Create two missing foods
+    bread = FactoryBot.create(:food, name: 'Bread', price: 2.5)
+    milk = FactoryBot.create(:food, name: 'Milk', price: 3.5)
 
-    # visit shopping_list_path(recipe_id: recipe.id, inventory_id: inventory.id)
+    recipe.recipe_foods.create(food: bread, quantity: 1)
+    recipe.recipe_foods.create(food: milk, quantity: 2)
+
+    # Calculate the missing foods
+    recipe.recipe_foods.each do |recipe_food|
+      missing_quantity = recipe_food.quantity - inventory.inventory_foods.where(food: recipe_food.food).sum(:quantity)
+      if missing_quantity.positive?
+        missing_foods << { food: recipe_food.food, quantity_needed: missing_quantity }
+      end
+    end
+
+    visit shopping_list_path(recipe_id: recipe.id, inventory_id: inventory.id, selected_inventory_id: inventory.id)
   end
 
   scenario 'User views the shopping list details' do
     # Assertions to check the content of the page
-    expect(page).to have_content("Amount of food to buy: #{food1.name}: 2, #{food2.name}: 2, #{food3.name}: 5")
+    expect(page).to have_content('Shopping List')
+    expect(page).to have_content("Amount of food to buy: #{missing_foods.count}")
     expect(page).to have_link('Sample Recipe', href: recipe_path(recipe))
-    expect(page).to have_content("Total value of food needed: $#{(food1.price * 2 + food2.price * 2 + food3.price * 5).to_f}")
+    expect(page).to have_content("Total value of food needed: $#{(missing_foods.map { |missing_food| missing_food[:food].price * missing_food[:quantity_needed] }.sum).to_f}")
     expect(page).to have_link('Sample Inventory', href: inventory_path(inventory))
 
     within('table') do
-      expect(page).to have_selector('thead')
-      expect(page).to have_selector('tbody')
-
-      expect(page).to have_content('Serial#')
-      expect(page).to have_content('Food')
-      expect(page).to have_content('Quantity')
-      expect(page).to have_content('Price')
-
+      expect(page).to have_content('Bread')
       expect(page).to have_content('1')
-      expect(page).to have_content(food1.name)
-      expect(page).to have_content('2')
-      expect(page).to have_content("$#{(food1.price * 2).to_f}")
+      expect(page).to have_content('$2.5')
 
+      expect(page).to have_content('Milk')
       expect(page).to have_content('2')
-      expect(page).to have_content(food2.name)
-      expect(page).to have_content('2')
-      expect(page).to have_content("$#{(food2.price * 2).to_f}")
-
-      expect(page).to have_content('3')
-      expect(page).to have_content(food3.name)
-      expect(page).to have_content('5')
-      expect(page).to have_content("$#{(food3.price * 5).to_f}")
+      expect(page).to have_content('$7.0')
     end
   end
 end
