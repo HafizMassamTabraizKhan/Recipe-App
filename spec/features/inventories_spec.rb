61ed4c3d4a135b86821d06c8a0b55e19d4ccd0c8
Peter Dumbari <dummytwiz@gmail.com>
2023-09-21 16:54:53 +0100

    added integration test for inventories

--- spec/features/inventories_spec.rb
+++ spec/features/inventories_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
-RSpec.feature "Inventories", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature 'Inventory Index', type: :feature do
+
+  let(:inventory) { create(:inventory) }
+
+  before do
+    create(:inventory_food, inventory: inventory, food: create(:food))
+    visit inventory_path(inventory)
+  end
+
+  scenario 'shows the inventory name' do
+    expect(page).to have_content(inventory.name)
+  end
+
+  scenario 'has an add food link' do
+    expect(page).to have_link('Add Food', href: new_inventory_inventory_food_path(inventory))
+  end
+
+  scenario 'shows the inventory foods' do
+    expect(page).to have_content(inventory.inventory_foods.first.food.name)
+  end
 end
