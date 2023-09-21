f3a34a109eb740c7e0c6dda8e946ce41304a14e8
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-09-21 19:41:54 +0500

    Modify food model to make all test pass

--- app/models/food.rb
+++ app/models/food.rb
@@ -5,15 +5,18 @@ class Food < ApplicationRecord
   has_many :recipe_foods, dependent: :destroy
   has_many :recipes, through: :recipe_foods
 
-  validates :name, uniqueness: true, presence: true
+  # validates :name, uniqueness: true, presence: true
+  # validate :name_is_unique_case_insensitive, on: :create
+
+  validates :name, uniqueness: { case_sensitive: false }, presence: true
   validates :measurement_unit, presence: true
-  validate :name_is_unique_case_insensitive, on: :create
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
   private
 
   def name_is_unique_case_insensitive
-    return unless Food.where('LOWER(name) = ?', name.downcase).exists?
+    # return unless Food.where('LOWER(name) = ?', name.downcase).exists?
+    return unless name && Food.where('LOWER(name) = ?', name.downcase).exists?
 
     errors.add(:name, 'has already been taken (case-insensitive)')
   end
