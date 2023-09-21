6a5aedc82c13fd0d50c21cce1af65f6b730139b7
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-09-21 20:43:15 +0500

    Add styling to add food form

--- app/models/food.rb
+++ app/models/food.rb
@@ -5,9 +5,6 @@ class Food < ApplicationRecord
   has_many :recipe_foods, dependent: :destroy
   has_many :recipes, through: :recipe_foods
 
-  # validates :name, uniqueness: true, presence: true
-  # validate :name_is_unique_case_insensitive, on: :create
-
   validates :name, uniqueness: { case_sensitive: false }, presence: true
   validates :measurement_unit, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
@@ -15,7 +12,6 @@ class Food < ApplicationRecord
   private
 
   def name_is_unique_case_insensitive
-    # return unless Food.where('LOWER(name) = ?', name.downcase).exists?
     return unless name && Food.where('LOWER(name) = ?', name.downcase).exists?
 
     errors.add(:name, 'has already been taken (case-insensitive)')
