4f19bd47d2eb7e323902f908c9916f6dc34c3e33
Peter Dumbari <dummytwiz@gmail.com>
2023-09-21 15:11:27 +0100

    added integration test for food

--- spec/features/foods_spec.rb
+++ spec/features/foods_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
-RSpec.feature "Foods", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature 'Food Details', type: :feature do
+  let(:food) { create(:food) }
+
+  before do
+    visit food_path(food)
+  end
+
+  scenario 'shows the food details' do
+    expect(page).to have_content(food.name)
+    expect(page).to have_content(food.measurement_unit)
+    expect(page).to have_content("$ #{food.price}")
+  end
+
+  scenario 'has a back link' do
+    expect(page).to have_link('Back', href: foods_path)
+  end
 end
